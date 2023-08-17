fc1bc22d4cbafbd5d4aa252075cd76cfa2f8e083
artyo_Om <maklakov.am@phystech.edu>
2023-08-17 18:58:20 +0300

    update 1.2
    fixed comments

--- main.cpp
+++ main.cpp
@@ -13,65 +13,65 @@ int main(int argc, char *argv[])			// for input from file type direction in term
 {
   int times_run = 1;
   while (true) {
-	float a, b, c;
-	if (argc == 1 || times_run > 1) 		// input from stdin
-	 {
-	  printf("Equation format: ax^2 + bx + c = 0\n");
-	  if (!GetCoef(&a, 'a'))				// getting a coefficient
-	  {
-		return 1;							// failed to get a coefficient
-	  }
-	  if (!GetCoef(&b, 'b'))				// getting b coefficient
-	  {
-		return 1;							// failed to get b coefficient
-	  }
-	  if (!GetCoef(&c, 'c'))				// getting c coefficient
-	  {
-		return 1;							// failed to get c coefficient
-	  }
-	} else {	 							// file input case
-	  FILE *fp = fopen(argv[1], "r");
-	  if (!fp)
-	  {
-		printf("Failed to open file: %s\n", argv[1]);
-		return 1;
-	  } else
-	  {
-		FSkipFormatText(fp);				// skipping "a =" part
-		if (!FGetCoef(fp, &a))  			// getting a coefficient from file
-		{
-		  return 1; 						// failed to get coefficient
-		}
-		fgetc(fp);
-		FSkipFormatText(fp);				// skipping '\n' char
-		if (!FGetCoef(fp, &b))  			// getting b coefficient from file
-		{
-		  return 1; 						// failed to get coefficient
-		}
-		fgetc(fp);
-		FSkipFormatText(fp);
-		if (!FGetCoef(fp, &c))  			// getting c coefficient from file
-		{
-		  return 1; 						// failed to get coefficient
-		}
-		if (fclose(fp))
-		{
-		  printf("Failed to close file: %s\n", argv[1]);
-		}
-	  }
-	}
-	struct solve * ans;
-	ans = QuadSolver(a, b, c);  									// solving part
-	PrintRoots(ans->amount, ans->first, ans->second);   			// printing roots
-	printf("Do you want to solve another equation? (1 - Yes): ");
-	int repeat_flag = 0;
-	scanf("%d", &repeat_flag);  									// repeat conditions
-	ClearInput();
-	if (!repeat_flag)
-	{
-	  break;
-	}
-	times_run++;									// solved equations counter
+    float a, b, c;
+    if (argc == 1 || times_run > 1) 		// input from stdin
+     {
+      printf("Equation format: ax^2 + bx + c = 0\n");
+      if (!GetCoef(&a, 'a'))				// getting a coefficient
+      {
+        return 1;							// failed to get a coefficient
+      }
+      if (!GetCoef(&b, 'b'))				// getting b coefficient
+      {
+        return 1;							// failed to get b coefficient
+      }
+      if (!GetCoef(&c, 'c'))				// getting c coefficient
+      {
+        return 1;							// failed to get c coefficient
+      }
+    } else {	 							// file input case
+      FILE *fp = fopen(argv[1], "r");
+      if (!fp)
+      {
+        printf("Failed to open file: %s\n", argv[1]);
+        return 1;
+      } else
+      {
+        FSkipFormatText(fp);				// skipping "a =" part
+        if (!FGetCoef(fp, &a))  			// getting a coefficient from file
+        {
+          return 1; 						// failed to get coefficient
+        }
+        fgetc(fp);
+        FSkipFormatText(fp);				// skipping '\n' char
+        if (!FGetCoef(fp, &b))  			// getting b coefficient from file
+        {
+          return 1; 						// failed to get coefficient
+        }
+        fgetc(fp);
+        FSkipFormatText(fp);
+        if (!FGetCoef(fp, &c))              // getting c coefficient from file
+        {
+          return 1;                         // failed to get coefficient
+        }
+        if (fclose(fp))
+        {
+          printf("Failed to close file: %s\n", argv[1]);
+        }
+      }
+    }
+    struct solve * ans;
+    ans = QuadSolver(a, b, c);                                      // solving part
+    PrintRoots(ans->amount, ans->first, ans->second);               // printing roots
+    printf("Do you want to solve another equation? (1 - Yes): ");
+    int repeat_flag = 0;
+    scanf("%d", &repeat_flag);                                      // repeat conditions
+    ClearInput();
+    if (!repeat_flag)
+    {
+      break;
+    }
+    times_run++;                                                    // solved equations counter
   }
   printf("Bye Bye\n");
 }
--- solver.cpp
+++ solver.cpp
@@ -4,18 +4,18 @@
 
 #define EPSILON 1e-6
 
-int Compare(const float a, const float b)   	// comparing two float numbers with EPSILON accuracy
+int Compare(const float a, const float b)       // comparing two float numbers with EPSILON accuracy
 {
   float diff = a - b;
-  if (abs(diff) < EPSILON)  					// a = b conditions
+  if (abs(diff) < EPSILON)                      // a = b conditions
   {
-	return EQUAL;
-  } else if (diff > EPSILON)					// a > b conditions
+    return EQUAL;
+  } else if (diff > EPSILON)                    // a > b conditions
   {
-	return MORE;
-  } else if (diff < -EPSILON)   				// a < b conditions
+    return MORE;
+  } else if (diff < -EPSILON)                   // a < b conditions
   {
-	return LESS;
+    return LESS;
   }
 }
 // -----------------------------------------------------------------------------------------
@@ -25,36 +25,36 @@ struct solve * QuadSolver(const float a, const float b, const float c)
   struct solve * ptr = &ans;
   if (Compare(a, 0) == EQUAL)
   {
-	if (Compare(b, 0) == EQUAL)
-	{
-	  if (Compare(c, 0) == EQUAL)   			// 0 + 0 + 0 = 0
-	  {
-		ptr->amount = INF_ROOTS;
-	  } else {  								// 0 + 0 + c = 0
-		ptr->amount = ZERO_ROOTS;
-	  }
-	} else {									// bx + c = 0
-	  ptr->amount = ONE_ROOT;
-	  ptr->first = (-c / b);
-	}
-  } else										// ax^2 + bx + c = 0
+    if (Compare(b, 0) == EQUAL)
+    {
+      if (Compare(c, 0) == EQUAL)               // 0 + 0 + 0 = 0
+      {
+        ptr->amount = INF_ROOTS;
+      } else {                                  // 0 + 0 + c = 0
+        ptr->amount = ZERO_ROOTS;
+      }
+    } else {                                    // bx + c = 0
+      ptr->amount = ONE_ROOT;
+      ptr->first = (-c / b);
+    }
+  } else                                        // ax^2 + bx + c = 0
   {
-	float D;									// discriminant
-	D = b*b - 4*a*c;							// discriminant formula
-	if (Compare(D, 0) == LESS)  				// zero roots conditions
-	{
-	  ptr->amount = ZERO_ROOTS;
-	} else if (Compare(D, 0) == EQUAL)  		// one root condition
-	{
-	  ptr->amount = ONE_ROOT;
-	  ptr->first = (-b / (2 * a));
-	} else {									// two roots conditions
-	  float root1, root2;
-	  ptr->amount = TWO_ROOTS;
-	  float discriminant_root = sqrt(D);
-	  ptr->first = ((-b - discriminant_root) / (2 * a));
-	  ptr->second = ((-b + discriminant_root) / (2 * a));
-	}
+    float D;                                    // discriminant
+    D = b*b - 4*a*c;                            // discriminant formula
+    if (Compare(D, 0) == LESS)                  // zero roots conditions
+    {
+      ptr->amount = ZERO_ROOTS;
+    } else if (Compare(D, 0) == EQUAL)          // one root condition
+    {
+      ptr->amount = ONE_ROOT;
+      ptr->first = (-b / (2 * a));
+    } else {                                    // two roots conditions
+      float root1, root2;
+      ptr->amount = TWO_ROOTS;
+      float discriminant_root = sqrt(D);
+      ptr->first = ((-b - discriminant_root) / (2 * a));
+      ptr->second = ((-b + discriminant_root) / (2 * a));
+    }
   }
   return ptr;
 }
